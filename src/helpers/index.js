bddddbd3593b90bec6ed5978b1133ae84c960356
Facu Masino <facujmasino@gmail.com>
2022-05-26 23:52:32 -0300

    Favicon + OG Tags + helpers updated

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -35,3 +35,35 @@ export const workDays = [
 		closeHour: '23:00',
 	},
 ];
+
+const formatDate = (msDate) => {
+	const date = new Date(msDate);
+	const [day, month, year] = [date.getDate(), date.getMonth(), date.getFullYear()];
+	const [hour, mins] = [date.getHours(), date.getMinutes()];
+	return `${day}/${month}/${year} ${hour}:${mins} hs`;
+};
+
+export const createWppLink = (orderData) => {
+	const BASE_URL = 'https://wa.me/5492227516436?text=';
+	const msg =
+		`Hola! Esta es la orden *#${orderData.id}* de _PediMe_,` +
+		`generada hoy _${formatDate(orderData.date)}_ por *${orderData.buyer.name} ${
+			orderData.buyer.lastname
+		}*.\n` +
+		`A continuación los detalles del pedido:\n` +
+		`${orderData.items.map((item) => `- ${item.title} - $ ${item.price} x${item.quantity}\n`)}` +
+		`*Forma de pago:* ${orderData.buyer.payment}\n` +
+		`*TOTAL:* $ ${orderData.total}\n` +
+		`A continuación enviaré mi ubicación.`;
+	return BASE_URL + encodeURIComponent(msg);
+};
+
+export const openWpp = (orderData) => {
+	try {
+		let newTab = window.open();
+		newTab.location.href = createWppLink(orderData);
+	} catch {
+		// Evita un error en mobile cuándo se bloquea el popUp
+		console.log('No se pudo abrir WhatsApp');
+	}
+};
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -9,6 +9,7 @@ import CheckoutForm from '../../components/CheckoutForm';
 import CartEmpty from '../../components/CartEmpty';
 
 import { saveOrder } from '../../services';
+import { openWpp, createWppLink } from '../../helpers';
 
 const Checkout = () => {
 	const { getTotalQty, clear } = useContext(CartContext);
@@ -30,31 +31,6 @@ const Checkout = () => {
 		return countdown;
 	};
 
-	const createWppLink = (orderData) => {
-		const BASE_URL = 'https://wa.me/5492227516436?text=';
-		const msg =
-			`Hola! Esta es la orden *#${orderData.id}* de _PediMe_,` +
-			`generada hoy _${formatDate(orderData.date)}_ por *${orderData.buyer.name} ${
-				orderData.buyer.lastname
-			}*.\n` +
-			`A continuación los detalles del pedido:\n` +
-			`${orderData.items.map((item) => `- ${item.title} - $ ${item.price} x${item.quantity}\n`)}` +
-			`*Forma de pago:* ${orderData.buyer.payment}\n` +
-			`*TOTAL:* $ ${orderData.total}\n` +
-			`A continuación enviaré mi ubicación.`;
-		return BASE_URL + encodeURIComponent(msg);
-	};
-
-	const openWpp = () => {
-		try {
-			let newTab = window.open();
-			newTab.location.href = createWppLink(order);
-		} catch {
-			// Evita un error en mobile cuándo se bloquea el popUp
-			console.log('No se pudo abrir WhatsApp');
-		}
-	};
-
 	const handleCheckout = (orderData) => {
 		setIsProcessing(true);
 		saveOrder(orderData).then((response) => {
@@ -62,13 +38,6 @@ const Checkout = () => {
 			setOrder({ ...response, ...orderData });
 			setIsProcessing(false);
 		});
-	};
-
-	const formatDate = (msDate) => {
-		const date = new Date(msDate);
-		const [day, month, year] = [date.getDate(), date.getMonth(), date.getFullYear()];
-		const [hour, mins] = [date.getHours(), date.getMinutes()];
-		return `${day}/${month}/${year} ${hour}:${mins} hs`;
 	};
 
 	const exitCheckout = () => {
@@ -148,7 +117,7 @@ const Checkout = () => {
 										<h3 className="text-title">Orden #{order.id}</h3>
 										<p>
 											Serás redirigido a nuestro WhatsApp en{' '}
-											<Countdown seconds={3} action={() => openWpp()} /> segundos, si no ocurre{' '}
+											<Countdown seconds={3} action={() => openWpp(order)} /> segundos, si no ocurre{' '}
 											<a href={createWppLink(order)} target="_blank" rel="noreferrer">
 												haz click aquí
 											</a>
